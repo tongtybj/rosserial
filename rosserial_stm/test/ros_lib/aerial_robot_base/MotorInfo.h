6a9f460cb2755a991c9fc07bac6d06f9ccc4a9d7
tongtybj <chou@jsk.imi.i.u-tokyo.ac.jp>
2016-07-07 08:01:36 +0900

    change the motorinfo message

--- rosserial_stm/test/ros_lib/aerial_robot_base/MotorInfo.h
+++ rosserial_stm/test/ros_lib/aerial_robot_base/MotorInfo.h
@@ -12,12 +12,16 @@ namespace aerial_robot_base
   class MotorInfo : public ros::Msg
   {
     public:
+      float min_pwm;
+      float max_pwm;
       float f_pwm_rate;
       float f_pwm_offset;
       float m_f_rate;
       float pwm_rate;
 
     MotorInfo():
+      min_pwm(0),
+      max_pwm(0),
       f_pwm_rate(0),
       f_pwm_offset(0),
       m_f_rate(0),
@@ -28,6 +32,26 @@ namespace aerial_robot_base
     virtual int serialize(unsigned char *outbuffer) const
     {
       int offset = 0;
+      union {
+        float real;
+        uint32_t base;
+      } u_min_pwm;
+      u_min_pwm.real = this->min_pwm;
+      *(outbuffer + offset + 0) = (u_min_pwm.base >> (8 * 0)) & 0xFF;
+      *(outbuffer + offset + 1) = (u_min_pwm.base >> (8 * 1)) & 0xFF;
+      *(outbuffer + offset + 2) = (u_min_pwm.base >> (8 * 2)) & 0xFF;
+      *(outbuffer + offset + 3) = (u_min_pwm.base >> (8 * 3)) & 0xFF;
+      offset += sizeof(this->min_pwm);
+      union {
+        float real;
+        uint32_t base;
+      } u_max_pwm;
+      u_max_pwm.real = this->max_pwm;
+      *(outbuffer + offset + 0) = (u_max_pwm.base >> (8 * 0)) & 0xFF;
+      *(outbuffer + offset + 1) = (u_max_pwm.base >> (8 * 1)) & 0xFF;
+      *(outbuffer + offset + 2) = (u_max_pwm.base >> (8 * 2)) & 0xFF;
+      *(outbuffer + offset + 3) = (u_max_pwm.base >> (8 * 3)) & 0xFF;
+      offset += sizeof(this->max_pwm);
       union {
         float real;
         uint32_t base;
@@ -77,6 +101,28 @@ namespace aerial_robot_base
       union {
         float real;
         uint32_t base;
+      } u_min_pwm;
+      u_min_pwm.base = 0;
+      u_min_pwm.base |= ((uint32_t) (*(inbuffer + offset + 0))) << (8 * 0);
+      u_min_pwm.base |= ((uint32_t) (*(inbuffer + offset + 1))) << (8 * 1);
+      u_min_pwm.base |= ((uint32_t) (*(inbuffer + offset + 2))) << (8 * 2);
+      u_min_pwm.base |= ((uint32_t) (*(inbuffer + offset + 3))) << (8 * 3);
+      this->min_pwm = u_min_pwm.real;
+      offset += sizeof(this->min_pwm);
+      union {
+        float real;
+        uint32_t base;
+      } u_max_pwm;
+      u_max_pwm.base = 0;
+      u_max_pwm.base |= ((uint32_t) (*(inbuffer + offset + 0))) << (8 * 0);
+      u_max_pwm.base |= ((uint32_t) (*(inbuffer + offset + 1))) << (8 * 1);
+      u_max_pwm.base |= ((uint32_t) (*(inbuffer + offset + 2))) << (8 * 2);
+      u_max_pwm.base |= ((uint32_t) (*(inbuffer + offset + 3))) << (8 * 3);
+      this->max_pwm = u_max_pwm.real;
+      offset += sizeof(this->max_pwm);
+      union {
+        float real;
+        uint32_t base;
       } u_f_pwm_rate;
       u_f_pwm_rate.base = 0;
       u_f_pwm_rate.base |= ((uint32_t) (*(inbuffer + offset + 0))) << (8 * 0);
@@ -122,7 +168,7 @@ namespace aerial_robot_base
     }
 
     const char * getType(){ return "aerial_robot_base/MotorInfo"; };
-    const char * getMD5(){ return "0a32f33b9d1229250db1fed7eafb25f1"; };
+    const char * getMD5(){ return "d96fbf68c692e6345982dae5c4be9384"; };
 
   };
 
