45c8fd3aa5969deab3c1614abf9b4f19a64da01a
Tongtybj <chou@jsk.imi.i.u-tokyo.ac.jp>
2016-12-15 00:46:44 +0900

    Modified several ros message, mainly resizing the large topic

--- rosserial_stm/test/ros_lib/aerial_robot_msgs/RollPitchYawGain.h
+++ rosserial_stm/test/ros_lib/aerial_robot_msgs/RollPitchYawGain.h
@@ -12,13 +12,13 @@ namespace aerial_robot_msgs
   class RollPitchYawGain : public ros::Msg
   {
     public:
-      float roll_p_gain[6];
-      float roll_i_gain[6];
-      float roll_d_gain[6];
-      float pitch_p_gain[6];
-      float pitch_i_gain[6];
-      float pitch_d_gain[6];
-      float yaw_d_gain[6];
+      int16_t roll_p_gain[6];
+      int16_t roll_i_gain[6];
+      int16_t roll_d_gain[6];
+      int16_t pitch_p_gain[6];
+      int16_t pitch_i_gain[6];
+      int16_t pitch_d_gain[6];
+      int16_t yaw_d_gain[6];
 
     RollPitchYawGain():
       roll_p_gain(),
@@ -36,86 +36,72 @@ namespace aerial_robot_msgs
       int offset = 0;
       for( uint8_t i = 0; i < 6; i++){
       union {
-        float real;
-        uint32_t base;
+        int16_t real;
+        uint16_t base;
       } u_roll_p_gaini;
       u_roll_p_gaini.real = this->roll_p_gain[i];
       *(outbuffer + offset + 0) = (u_roll_p_gaini.base >> (8 * 0)) & 0xFF;
       *(outbuffer + offset + 1) = (u_roll_p_gaini.base >> (8 * 1)) & 0xFF;
-      *(outbuffer + offset + 2) = (u_roll_p_gaini.base >> (8 * 2)) & 0xFF;
-      *(outbuffer + offset + 3) = (u_roll_p_gaini.base >> (8 * 3)) & 0xFF;
       offset += sizeof(this->roll_p_gain[i]);
       }
       for( uint8_t i = 0; i < 6; i++){
       union {
-        float real;
-        uint32_t base;
+        int16_t real;
+        uint16_t base;
       } u_roll_i_gaini;
       u_roll_i_gaini.real = this->roll_i_gain[i];
       *(outbuffer + offset + 0) = (u_roll_i_gaini.base >> (8 * 0)) & 0xFF;
       *(outbuffer + offset + 1) = (u_roll_i_gaini.base >> (8 * 1)) & 0xFF;
-      *(outbuffer + offset + 2) = (u_roll_i_gaini.base >> (8 * 2)) & 0xFF;
-      *(outbuffer + offset + 3) = (u_roll_i_gaini.base >> (8 * 3)) & 0xFF;
       offset += sizeof(this->roll_i_gain[i]);
       }
       for( uint8_t i = 0; i < 6; i++){
       union {
-        float real;
-        uint32_t base;
+        int16_t real;
+        uint16_t base;
       } u_roll_d_gaini;
       u_roll_d_gaini.real = this->roll_d_gain[i];
       *(outbuffer + offset + 0) = (u_roll_d_gaini.base >> (8 * 0)) & 0xFF;
       *(outbuffer + offset + 1) = (u_roll_d_gaini.base >> (8 * 1)) & 0xFF;
-      *(outbuffer + offset + 2) = (u_roll_d_gaini.base >> (8 * 2)) & 0xFF;
-      *(outbuffer + offset + 3) = (u_roll_d_gaini.base >> (8 * 3)) & 0xFF;
       offset += sizeof(this->roll_d_gain[i]);
       }
       for( uint8_t i = 0; i < 6; i++){
       union {
-        float real;
-        uint32_t base;
+        int16_t real;
+        uint16_t base;
       } u_pitch_p_gaini;
       u_pitch_p_gaini.real = this->pitch_p_gain[i];
       *(outbuffer + offset + 0) = (u_pitch_p_gaini.base >> (8 * 0)) & 0xFF;
       *(outbuffer + offset + 1) = (u_pitch_p_gaini.base >> (8 * 1)) & 0xFF;
-      *(outbuffer + offset + 2) = (u_pitch_p_gaini.base >> (8 * 2)) & 0xFF;
-      *(outbuffer + offset + 3) = (u_pitch_p_gaini.base >> (8 * 3)) & 0xFF;
       offset += sizeof(this->pitch_p_gain[i]);
       }
       for( uint8_t i = 0; i < 6; i++){
       union {
-        float real;
-        uint32_t base;
+        int16_t real;
+        uint16_t base;
       } u_pitch_i_gaini;
       u_pitch_i_gaini.real = this->pitch_i_gain[i];
       *(outbuffer + offset + 0) = (u_pitch_i_gaini.base >> (8 * 0)) & 0xFF;
       *(outbuffer + offset + 1) = (u_pitch_i_gaini.base >> (8 * 1)) & 0xFF;
-      *(outbuffer + offset + 2) = (u_pitch_i_gaini.base >> (8 * 2)) & 0xFF;
-      *(outbuffer + offset + 3) = (u_pitch_i_gaini.base >> (8 * 3)) & 0xFF;
       offset += sizeof(this->pitch_i_gain[i]);
       }
       for( uint8_t i = 0; i < 6; i++){
       union {
-        float real;
-        uint32_t base;
+        int16_t real;
+        uint16_t base;
       } u_pitch_d_gaini;
       u_pitch_d_gaini.real = this->pitch_d_gain[i];
       *(outbuffer + offset + 0) = (u_pitch_d_gaini.base >> (8 * 0)) & 0xFF;
       *(outbuffer + offset + 1) = (u_pitch_d_gaini.base >> (8 * 1)) & 0xFF;
-      *(outbuffer + offset + 2) = (u_pitch_d_gaini.base >> (8 * 2)) & 0xFF;
-      *(outbuffer + offset + 3) = (u_pitch_d_gaini.base >> (8 * 3)) & 0xFF;
       offset += sizeof(this->pitch_d_gain[i]);
       }
       for( uint8_t i = 0; i < 6; i++){
       union {
-        float real;
-        uint32_t base;
+        int16_t real;
+        uint16_t base;
       } u_yaw_d_gaini;
       u_yaw_d_gaini.real = this->yaw_d_gain[i];
       *(outbuffer + offset + 0) = (u_yaw_d_gaini.base >> (8 * 0)) & 0xFF;
       *(outbuffer + offset + 1) = (u_yaw_d_gaini.base >> (8 * 1)) & 0xFF;
-      *(outbuffer + offset + 2) = (u_yaw_d_gaini.base >> (8 * 2)) & 0xFF;
-      *(outbuffer + offset + 3) = (u_yaw_d_gaini.base >> (8 * 3)) & 0xFF;
       offset += sizeof(this->yaw_d_gain[i]);
       }
       return offset;
@@ -126,92 +112,78 @@ namespace aerial_robot_msgs
       int offset = 0;
       for( uint8_t i = 0; i < 6; i++){
       union {
-        float real;
-        uint32_t base;
+        int16_t real;
+        uint16_t base;
       } u_roll_p_gaini;
       u_roll_p_gaini.base = 0;
-      u_roll_p_gaini.base |= ((uint32_t) (*(inbuffer + offset + 0))) << (8 * 0);
-      u_roll_p_gaini.base |= ((uint32_t) (*(inbuffer + offset + 1))) << (8 * 1);
-      u_roll_p_gaini.base |= ((uint32_t) (*(inbuffer + offset + 2))) << (8 * 2);
-      u_roll_p_gaini.base |= ((uint32_t) (*(inbuffer + offset + 3))) << (8 * 3);
+      u_roll_p_gaini.base |= ((uint16_t) (*(inbuffer + offset + 0))) << (8 * 0);
+      u_roll_p_gaini.base |= ((uint16_t) (*(inbuffer + offset + 1))) << (8 * 1);
       this->roll_p_gain[i] = u_roll_p_gaini.real;
       offset += sizeof(this->roll_p_gain[i]);
       }
       for( uint8_t i = 0; i < 6; i++){
       union {
-        float real;
-        uint32_t base;
+        int16_t real;
+        uint16_t base;
       } u_roll_i_gaini;
       u_roll_i_gaini.base = 0;
-      u_roll_i_gaini.base |= ((uint32_t) (*(inbuffer + offset + 0))) << (8 * 0);
-      u_roll_i_gaini.base |= ((uint32_t) (*(inbuffer + offset + 1))) << (8 * 1);
-      u_roll_i_gaini.base |= ((uint32_t) (*(inbuffer + offset + 2))) << (8 * 2);
-      u_roll_i_gaini.base |= ((uint32_t) (*(inbuffer + offset + 3))) << (8 * 3);
+      u_roll_i_gaini.base |= ((uint16_t) (*(inbuffer + offset + 0))) << (8 * 0);
+      u_roll_i_gaini.base |= ((uint16_t) (*(inbuffer + offset + 1))) << (8 * 1);
       this->roll_i_gain[i] = u_roll_i_gaini.real;
       offset += sizeof(this->roll_i_gain[i]);
       }
       for( uint8_t i = 0; i < 6; i++){
       union {
-        float real;
-        uint32_t base;
+        int16_t real;
+        uint16_t base;
       } u_roll_d_gaini;
       u_roll_d_gaini.base = 0;
-      u_roll_d_gaini.base |= ((uint32_t) (*(inbuffer + offset + 0))) << (8 * 0);
-      u_roll_d_gaini.base |= ((uint32_t) (*(inbuffer + offset + 1))) << (8 * 1);
-      u_roll_d_gaini.base |= ((uint32_t) (*(inbuffer + offset + 2))) << (8 * 2);
-      u_roll_d_gaini.base |= ((uint32_t) (*(inbuffer + offset + 3))) << (8 * 3);
+      u_roll_d_gaini.base |= ((uint16_t) (*(inbuffer + offset + 0))) << (8 * 0);
+      u_roll_d_gaini.base |= ((uint16_t) (*(inbuffer + offset + 1))) << (8 * 1);
       this->roll_d_gain[i] = u_roll_d_gaini.real;
       offset += sizeof(this->roll_d_gain[i]);
       }
       for( uint8_t i = 0; i < 6; i++){
       union {
-        float real;
-        uint32_t base;
+        int16_t real;
+        uint16_t base;
       } u_pitch_p_gaini;
       u_pitch_p_gaini.base = 0;
-      u_pitch_p_gaini.base |= ((uint32_t) (*(inbuffer + offset + 0))) << (8 * 0);
-      u_pitch_p_gaini.base |= ((uint32_t) (*(inbuffer + offset + 1))) << (8 * 1);
-      u_pitch_p_gaini.base |= ((uint32_t) (*(inbuffer + offset + 2))) << (8 * 2);
-      u_pitch_p_gaini.base |= ((uint32_t) (*(inbuffer + offset + 3))) << (8 * 3);
+      u_pitch_p_gaini.base |= ((uint16_t) (*(inbuffer + offset + 0))) << (8 * 0);
+      u_pitch_p_gaini.base |= ((uint16_t) (*(inbuffer + offset + 1))) << (8 * 1);
       this->pitch_p_gain[i] = u_pitch_p_gaini.real;
       offset += sizeof(this->pitch_p_gain[i]);
       }
       for( uint8_t i = 0; i < 6; i++){
       union {
-        float real;
-        uint32_t base;
+        int16_t real;
+        uint16_t base;
       } u_pitch_i_gaini;
       u_pitch_i_gaini.base = 0;
-      u_pitch_i_gaini.base |= ((uint32_t) (*(inbuffer + offset + 0))) << (8 * 0);
-      u_pitch_i_gaini.base |= ((uint32_t) (*(inbuffer + offset + 1))) << (8 * 1);
-      u_pitch_i_gaini.base |= ((uint32_t) (*(inbuffer + offset + 2))) << (8 * 2);
-      u_pitch_i_gaini.base |= ((uint32_t) (*(inbuffer + offset + 3))) << (8 * 3);
+      u_pitch_i_gaini.base |= ((uint16_t) (*(inbuffer + offset + 0))) << (8 * 0);
+      u_pitch_i_gaini.base |= ((uint16_t) (*(inbuffer + offset + 1))) << (8 * 1);
       this->pitch_i_gain[i] = u_pitch_i_gaini.real;
       offset += sizeof(this->pitch_i_gain[i]);
       }
       for( uint8_t i = 0; i < 6; i++){
       union {
-        float real;
-        uint32_t base;
+        int16_t real;
+        uint16_t base;
       } u_pitch_d_gaini;
       u_pitch_d_gaini.base = 0;
-      u_pitch_d_gaini.base |= ((uint32_t) (*(inbuffer + offset + 0))) << (8 * 0);
-      u_pitch_d_gaini.base |= ((uint32_t) (*(inbuffer + offset + 1))) << (8 * 1);
-      u_pitch_d_gaini.base |= ((uint32_t) (*(inbuffer + offset + 2))) << (8 * 2);
-      u_pitch_d_gaini.base |= ((uint32_t) (*(inbuffer + offset + 3))) << (8 * 3);
+      u_pitch_d_gaini.base |= ((uint16_t) (*(inbuffer + offset + 0))) << (8 * 0);
+      u_pitch_d_gaini.base |= ((uint16_t) (*(inbuffer + offset + 1))) << (8 * 1);
       this->pitch_d_gain[i] = u_pitch_d_gaini.real;
       offset += sizeof(this->pitch_d_gain[i]);
       }
       for( uint8_t i = 0; i < 6; i++){
       union {
-        float real;
-        uint32_t base;
+        int16_t real;
+        uint16_t base;
       } u_yaw_d_gaini;
       u_yaw_d_gaini.base = 0;
-      u_yaw_d_gaini.base |= ((uint32_t) (*(inbuffer + offset + 0))) << (8 * 0);
-      u_yaw_d_gaini.base |= ((uint32_t) (*(inbuffer + offset + 1))) << (8 * 1);
-      u_yaw_d_gaini.base |= ((uint32_t) (*(inbuffer + offset + 2))) << (8 * 2);
-      u_yaw_d_gaini.base |= ((uint32_t) (*(inbuffer + offset + 3))) << (8 * 3);
+      u_yaw_d_gaini.base |= ((uint16_t) (*(inbuffer + offset + 0))) << (8 * 0);
+      u_yaw_d_gaini.base |= ((uint16_t) (*(inbuffer + offset + 1))) << (8 * 1);
       this->yaw_d_gain[i] = u_yaw_d_gaini.real;
       offset += sizeof(this->yaw_d_gain[i]);
       }
